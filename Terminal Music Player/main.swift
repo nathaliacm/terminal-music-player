39ba6101a5eeeb2b112a74f89c66e1669e9bfff3
Rudigus <rodrigo.matos9@hotmail.com>
2020-04-02 19:36:09 -0300

    funcionalidade de download adicionada

--- Terminal Music Player/main.swift
+++ Terminal Music Player/main.swift
@@ -19,7 +19,7 @@
 import AVFoundation
 import Foundation
 
-let bye = "Tchau pessoa!"
+let bye = "Tchau pessoa!\n"
 let musicFilesURL: URL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath).appendingPathComponent("Music Files", isDirectory: true)
 let jsonURL = musicFilesURL.appendingPathComponent("musics.json")
 
@@ -66,16 +66,27 @@ class Music: Codable {
             musicPlayer.play()
             print("\nReproduzindo \(self.artist) - \(self.title)")
             while musicPlayer.isPlaying {
-                print("Digite \"stop\" para parar")
+                print("\nDigite \"stop\" para parar\n")
                 if let command = readLine(), command == "stop" {
                     print("\nMúsica encerrada")
                     break
                 } else {
-                    print("Comando inválido", terminator: " ")
+                    print("\nComando inválido\n", terminator: " ")
                 }
             }
         } catch {
             print("\nNão foi possível reproduzir a música")
+        }
+    }
+    
+    public func download(to fileURL: URL) {
+        do {
+            let musicData = try Data(contentsOf: self.url)
+            try musicData.write(to: fileURL)
+            print("\nMúsica baixada com sucesso")
+            
+        } catch {
+            print("\nNão foi possível baixar a música")
         }
     }
 }
@@ -90,6 +101,7 @@ func main()
     Salvar música - save
     Exibir músicas salvas - show
     Remover música - remove
+    Baixar música - download
     Descrição dos comandos - help
     Sair do Player - exit
 
@@ -105,34 +117,47 @@ func main()
     [entrada] - uma URL de um arquivo de música, o nome de uma música armazenada no diretório Music Files ou o nome de uma música salva com o comando save
     
     """
+    let musicError = "\nMúsica não encontrada\n"
     
     print(menu)
     while let command = readLine() {
-        // Adiciona uma linha vazia por motivos de estética
+        clearTerminalScreen()
         print("")
-        switch command {
+        print(menu)
+        switch command.lowercased() {
         case "play":
             let musicOpt = loadMusic()
             if let music = musicOpt {
                 music.play()
+                print("")
             } else {
-                print("\nMúsica não encontrada")
+                print(musicError)
             }
-            print(menu)
         case "save":
             let musicOpt = loadMusic()
             if let music = musicOpt {
                 saveMusic(music: music, fileURL: musicFilesURL.appendingPathComponent("Musics.json"))
             } else {
-                print("Entrada inválida")
+                print(musicError)
             }
-            print(menu)
         case "show":
             showMusics()
-            print(menu)
+            print("")
         case "remove":
             removeMusic()
-            print(menu)
+            print("")
+        case "download":
+            let musicOpt = loadMusic()
+            if let music = musicOpt {
+                /*
+                Mesmo que o arquivo de música fornecido não seja do tipo mp3, não terá problema. Como Mac é baseado em Unix, a extensão só servirá para o Mac atribuir um programa padrão para abrir esse arquivo.
+                 */
+                music.download(to:
+                musicFilesURL.appendingPathComponent("\(music.artist) - \(music.title).mp3"))
+                print("")
+            } else {
+                print(musicError)
+            }
         case "help":
             print(help)
         case "exit":
@@ -144,54 +169,9 @@ func main()
     }
 }
 
-// Mostra todas as músicas salvas no Player
-func showMusics() {
-    let dictMusicOpt = getDictFromFile()
-    // Checa se o dicionário é nulo ou vazio
-    guard let dictMusic = dictMusicOpt, !dictMusic.isEmpty else {
-        print("Nenhuma música salva")
-        return
-    }
-    for music in dictMusic.values {
-        print("\(music.artist) - \(music.title)")
-    }
-}
-
-// Solicita um nome de música e remove a referência da música que possui esse nome, caso exista
-func removeMusic() {
-    print("Informe o nome da música: ")
-    guard let musicName = readLine() else {
-        return
-    }
-    // Pula uma linha por questões estéticas
-    print("")
-    let dictMusicOpt = getDictFromFile()
-    guard var dictMusic = dictMusicOpt else {
-        print("Nenhuma música salva")
-        return
-    }
-    if dictMusic[musicName] != nil {
-        dictMusic[musicName] = nil
-        let jsonDataOpt = musicsToJsonData(musics: dictMusic)
-        guard let jsonData = jsonDataOpt else {
-            print("(jsonConversionError)")
-            return
-        }
-        do {
-            try jsonData.write(to: jsonURL)
-        } catch {
-            print("Não foi possível remover a música")
-            return
-        }
-        print("Música removida com sucesso")
-    } else {
-        print("Música inexistente")
-    }
-}
-
 // Solicita um arquivo de música e retorna um objeto música (optional)
 func loadMusic() -> Music? {
-    print("Informe a música: ")
+    print("Informe a música:\n")
     guard let musicInput = readLine() else {
         return nil
     }
@@ -221,7 +201,7 @@ func loadMusic() -> Music? {
 // Salva as informações de uma música em um arquivo .json
 func saveMusic(music: Music, fileURL: URL) {
     let saveError = "\nNão foi possível salvar a música"
-    let saveSuccess = "\nMúsica salva com sucesso"
+    let saveSuccess = "\nMúsica salva com sucesso\n"
     let fileManager = FileManager.default
     if fileManager.fileExists(atPath: fileURL.path) {
         let dictMusicOpt = getDictFromFile()
@@ -244,6 +224,51 @@ func saveMusic(music: Music, fileURL: URL) {
     } else {
         let createSuccess = createInitialFile(initialMusic: [music.title: music], fileURL: fileURL)
         print(createSuccess ? saveSuccess : saveError)
+    }
+}
+
+// Mostra todas as músicas salvas no Player
+func showMusics() {
+    let dictMusicOpt = getDictFromFile()
+    // Checa se o dicionário é nulo ou vazio
+    guard let dictMusic = dictMusicOpt, !dictMusic.isEmpty else {
+        print("Nenhuma música salva")
+        return
+    }
+    print("--- Lista de Músicas ---\n")
+    for music in dictMusic.values {
+        print("\(music.artist) - \(music.title)")
+    }
+}
+
+// Solicita um nome de música e remove a referência da música que possui esse nome, caso exista
+func removeMusic() {
+    print("Informe o nome da música:\n")
+    guard let musicName = readLine() else {
+        return
+    }
+    print("")
+    let dictMusicOpt = getDictFromFile()
+    guard var dictMusic = dictMusicOpt else {
+        print("Nenhuma música salva")
+        return
+    }
+    if dictMusic[musicName] != nil {
+        dictMusic[musicName] = nil
+        let jsonDataOpt = musicsToJsonData(musics: dictMusic)
+        guard let jsonData = jsonDataOpt else {
+            print("(jsonConversionError)")
+            return
+        }
+        do {
+            try jsonData.write(to: jsonURL)
+        } catch {
+            print("Não foi possível remover a música")
+            return
+        }
+        print("Música removida com sucesso")
+    } else {
+        print("Música inexistente")
     }
 }
 
@@ -274,6 +299,8 @@ func createInitialFile(initialMusic: [String: Music], fileURL: URL) -> Bool {
     //print("arquivo criado")
 }
 
+// Funções auxiliares (de conversão)
+
 // Converte json data para um dicionário de música (optional)
 func jsonDataToMusics(jsonData: Data) -> [String: Music]? {
     let jsonDecoder = JSONDecoder()
@@ -294,6 +321,15 @@ func musicsToJsonData(musics: [String: Music]) -> Data? {
     } catch {
         return nil
     }
+}
+
+// Limpa o terminal
+func clearTerminalScreen() {
+    let clear = Process()
+    clear.launchPath = "/usr/bin/clear"
+    clear.arguments = []
+    clear.launch()
+    clear.waitUntilExit()
 }
 
 main()
